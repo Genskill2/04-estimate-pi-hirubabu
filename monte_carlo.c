dc0baac4a62be3bcbebd1851d280c2826ebb8044
hirubabu <84659874+hirubabu@users.noreply.github.com>
2021-06-04 04:14:29 +0400

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,17 +38,18 @@ int main(void) {
     }
   }
 }
-float mc_pi(int n)
-{ float pi,x,y,z;
- int count=0;
- for(int i = 0; i < n; ++i) {
-   x = frandom();
-   y = frandom();
-   z = x * x + y * y;
-   if(z <= 1) count++;
-}
- pi = (double) count / n * 4;
- return pi;
+float mc_pi(int a){
+    int s1=0;
+    float pi;
+   for (int i=0; i<a; ++i) {
+     float x=frandom();
+     float y=frandom();
+     float d=x*x + y*y;
+       if (d<=1)
+         s1=s1+1;
+   }
+   pi= (double) (s1*4)/a;
+   return pi;
 }
 
 
